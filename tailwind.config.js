2784991564d90e2f0bf9da0298168ef8242b0750
Toni <tonichiga@gmail.com>
2023-07-28 21:28:02 +0300

    update dep, added tw example config, add sass dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    backgroundImage: {
+      "hero-mobile": "url('/images/hero-mobile.jpeg')", // example
+    },
+    colors: {
+      "text-primary": "#ffffff", // example
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {},
+
+      borderRadius: {
+        48: "48px",
+      },
+      fontSize: {
+        13: "13px",
+        32: "32px",
+        64: "64px",
+      },
+      lineHeight: {
+        105: "105%",
+        130: "130%",
+      },
+      fontWeight: {
+        400: 400,
+        500: 500,
+        600: 600,
+        700: 700,
+        800: 800,
+      },
+
+      screens: {
+        "max-mobile": { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+        "max-tablet": { max: "1339px" },
+        // => @media (max-width: 1339px) { ... }
+        "max-laptop": { max: "1919px" },
+        // => @media (max-width: 1919px) { ... }
+        tablet: "768px",
+        // => @media (min-width: 768px) { ... }
+        laptop: "1440px",
+        // => @media (min-width: 1440px) { ... }
+        desktop: "1920px",
+        // => @media (min-width: 1920px) { ... }
       },
     },
   },
   plugins: [],
-}
+};
